8e5e08f5f69389ba20aa0ecaf2e58b2083aa83c5
Madankumar06 <madankumar.p@navabrindit.com>
2023-04-07 19:14:12 +0530

    json data implementing

--- src/components/ProductSlider/ProductSlider.js
+++ src/components/ProductSlider/ProductSlider.js
@@ -4,7 +4,7 @@ import useObjectMap from "../../hooks/useObjectMap";
 import { Container, Box } from "../FlexContainer";
 import ProductSliderCard from "../ProductSliderCard/ProductSliderCard";
 import style from "./ProductSlider.module.css";
-
+import ProductCartData from "../../JsonData/productCartData.json";
 function NextArrow(props) {
   const { className, style, onClick } = props;
   return (
@@ -19,7 +19,7 @@ function NextArrow(props) {
         backgroundSize: "cover",
         zIndex: "1",
         top: "50%",
-        transform:' translateY(-50%)',
+        transform: " translateY(-50%)",
         right: "10px",
       }}
       onClick={onClick}
@@ -41,7 +41,7 @@ function PrevArrow(props) {
         backgroundSize: "cover",
         zIndex: "1",
         top: "50%",
-        transform: 'translateY(-50%)',
+        transform: "translateY(-50%)",
         left: "-10px",
       }}
       onClick={onClick}
@@ -58,20 +58,68 @@ const ProductSlider = () => {
     slidesToShow: 4,
     slidesToScroll: 1,
     prevArrow: <PrevArrow />,
-    nextArrow: <NextArrow /> ,
+    nextArrow: <NextArrow />,
   };
-    return (
-      
-      <Slider {...settings}>
 
-            <Box><ProductSliderCard needSpace /></Box>
-            <Box><ProductSliderCard needSpace /></Box>
-            <Box><ProductSliderCard needSpace/></Box>
-            <Box><ProductSliderCard needSpace/></Box>
-            <Box><ProductSliderCard needSpace/></Box>
-              
-    </Slider>
-  );
+  const iconMap = (item) => ({
+    ...item,
+    displaySrc: item?.image?.url,
+    displaytext: item?.name,
+    displayAlt: item?.name,
+    displaySpecialPrice: item?.special_price,
+    displayStockStatus: item?.stock_status,
+    displayRatingSummary: item?.rating_summary,
+    displayReviewCount: item?.review_count,
+    displayPriceRange: item?.price_range,
+    displayAdditionalFields: item?.additionalFields,
+    displayProductText: item?.additionalFields?.brand,
+    displayProductLeftText: `${item?.additionalFields?.qty_left}+ Kvar`,
+    displayPrice: `${
+      item?.special_price
+        ? item?.special_price
+        : item?.special_price
+        ? item?.special_price
+        : item?.price_range?.minimum_price?.regular_price?.value
+    } kr`,
+    displayOfferStrikeText: `${item?.price_range?.minimum_price?.regular_price?.value} kr`,
+    displaySoldyToday: `${item?.additionalFields?.qty_sold_today} Sålda idag`,
+    displayDiscount: Math.round(
+      ((item?.price_range?.minimum_price?.regular_price?.value -
+        item?.special_price) /
+        item?.price_range?.minimum_price?.regular_price?.value) *
+        100
+    ),
+    displayRatingCount: item?.review_count,
+    displayDateText:
+      item?.additionalFields?.delivery_date_max == "No Date Available" &&
+      item?.additionalFields?.delivery_date_min == "No Date Available"
+        ? "No Date Available"
+        : item?.additionalFields?.delivery_date_max ===
+          item?.additionalFields?.delivery_date_min
+        ? item?.additionalFields?.delivery_date_max
+        : `${item?.additionalFields?.delivery_date_max} to ${item?.additionalFields?.delivery_date_max}`,
+  });
+
+  const mappedData = useObjectMap(ProductCartData?.products?.items, iconMap);
+  const displayIcon = mappedData?.map((item) => (
+    <Box key={item?.url_key} style={{ width: "180px" }}>
+      <ProductSliderCard
+        productimageSrc={item?.displaySrc}
+        productimageAlt={item?.displayProductText}
+        productText={item?.displayProductText}
+        productLeftedtext={item?.displayProductLeftText}
+        OfferStrikeText={item?.displayOfferStrikeText}
+        PriceText={item?.displayPrice}
+        DiscriptionText={item?.displaytext}
+        ratingCount={item?.displayRatingCount}
+        soldtext={item?.displaySoldyToday}
+        discount={`${item?.displayDiscount}%`}
+        DateText={item?.displayDateText}
+        needSpace={false}
+      />
+    </Box>
+  ));
+  return <Slider {...settings}>{displayIcon}</Slider>;
 };
 
 export default ProductSlider;
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,47 +1,47 @@
-// import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+// // import Swiper core and required modules
+// import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import './Slider.css'; 
+// // Import Swiper styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+// import 'swiper/css/scrollbar';
+// import './Slider.css'; 
 
-const Slider = () => {
+// const Slider = () => {
     
-        const params = {
-          autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-          },
-        };
-  return (
-    <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={0}
-      slidesPerView={1}
-          navigation={true} 
-          autoplay={{
-            "delay": 2500,
-            "disableOnInteraction": false
-          }}
-      pagination={{ clickable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-      style={{ height: '500px', width: '800px' }} // add height and width
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      ...
-    </Swiper>
-  );
-};
+//         const params = {
+//           autoplay: {
+//             delay: 3000,
+//             disableOnInteraction: false,
+//           },
+//         };
+//   return (
+//     <Swiper
+//       modules={[Navigation, Pagination, Scrollbar, A11y]}
+//       spaceBetween={0}
+//       slidesPerView={1}
+//           navigation={true} 
+//           autoplay={{
+//             "delay": 2500,
+//             "disableOnInteraction": false
+//           }}
+//       pagination={{ clickable: true }}
+//       onSwiper={(swiper) => console.log(swiper)}
+//       onSlideChange={() => console.log('slide change')}
+//       style={{ height: '500px', width: '800px' }} // add height and width
+//     >
+//       <SwiperSlide>Slide 1</SwiperSlide>
+//       <SwiperSlide>Slide 2</SwiperSlide>
+//       ...
+//     </Swiper>
+//   );
+// };
 
-export default Slider;
+// export default Slider;
 
-Slider.propTypes = {
+// Slider.propTypes = {
     
-}
+// }
